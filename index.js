8e6e08833e1558ece98b7fd64e08bb7bc0f0b720
Bakiev Damir <8neto8@bk.ru>
2021-10-08 19:07:53 +0500

    added main function, added the function of getting popular movies, changed the function of updating db

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
 import { Client } from "@notionhq/client"
 import { config } from "dotenv"
+import fetch from "node-fetch"
 config()
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
-const databaseId = process.env.NOTION_DATABASE_ID
+const databaseId = process.env.NOTION_DATABASE_ID;
 
-async function addItem(text) {
+const keyTheMovieDb = process.env.KEY_THE_MOVIE_DB;
+
+main();
+
+async function main() {
   try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: { 
-          title:[
-            {
-              "text": {
-                "content": text
-              }
-            }
-          ]
-        }
-      },
-    })
-    console.log(response)
-    console.log("Success! Entry added.")
+    const popularFilms = await getPopularFilms();
+    await updateDB(popularFilms);
+    
   } catch (error) {
-    console.error(error.body)
+    console.log(error)
   }
 }
 
-addItem("Yurts in Big Sur, California")
+async function updateDB(popularFilms) {
+  let results = await Promise.all(
+    popularFilms.map((film) =>
+      notion.pages.create({
+        parent: { database_id: databaseId },
+        properties: getPropertiesFromFilm(film),
+      })
+    )
+  )
+  
+  console.log(results)
+  console.log("Success! Entry added.")
+}
+
+async function getPopularFilms() {
+  try {
+    const url = 'https://api.themoviedb.org/3/movie/popular?api_key=' + keyTheMovieDb;
+    const response = await fetch(url);
+    let json = await response.json();
+    return json.results;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getPropertiesFromFilm(film) {
+  const { id, original_title, overview, popularity, release_date, original_language } = film
+  return {
+    id: {
+      title: [{ type: "text", text: { content: String(id) } }],
+    },
+    original_title: {
+      rich_text: [ { type: "text", text: { content: original_title }}],
+    },
+    overview: {
+      rich_text: [ { type: "text", text: { content: overview }}],
+    },
+    popularity: {
+      number: popularity,
+    },
+    release_date: {
+      rich_text: [ { type: "text", text: { content: release_date }}],
+    },
+    original_language: {
+      rich_text: [ { type: "text", text: { content: original_language }}],
+    },
+  }
+}
